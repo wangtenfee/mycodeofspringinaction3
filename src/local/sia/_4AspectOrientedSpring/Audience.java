228f05eb407e86e70f47698879645fb8be7fa340
Administrator <Administrator@wtf-PC>
2016-08-17 10:37:46 +0800

    add springinactionNote.docx

--- src/local/sia/_4AspectOrientedSpring/Audience.java
+++ src/local/sia/_4AspectOrientedSpring/Audience.java
@@ -1,5 +1,21 @@
 package local.sia._4AspectOrientedSpring;
 
 public class Audience {
-
+	
+	public void takeSeats(){
+		System.out.println("audience is taking their seats...");
+	}
+	
+	public void turnOffCellPhones(){
+		System.out.println("audience is turning off their phone...");
+	}
+	
+	public void applaud(){
+		System.out.println("audience is applauding...CLAP CLAP CLAP CLAP CLAP ");
+	}
+	
+	public void demandRefund(){
+		System.out.println("Boo! We want our money back!");
+	}
+	
 }
